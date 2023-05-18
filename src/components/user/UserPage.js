d957fb424e3246ed7a27d0f6c56cb2ac87e3e1e3
charbel.f <charbel.f@i-stay.ltd>
2023-05-18 17:35:49 +0300

    delete modal confirmation

--- src/components/user/UserPage.js
+++ src/components/user/UserPage.js
@@ -1,11 +1,13 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import {Link, useHistory} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import UserService from "../../services/UserService";
-import {ToastContainer, toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AiOutlineUserAdd, AiFillDelete, AiFillEdit } from "react-icons/ai";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 //onCreation complete of this Component. The first method react useEffect
 
@@ -13,106 +15,104 @@ const UserPage = () => {
 
     const [persons, setPersons] = useState([]);
     const [person, setPerson] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedPersonId, setSelectedPersonId] = useState(0);
 
     const history = useHistory();
 
-    useEffect(() =>{
+    useEffect(() => {
         loadUsers();
     }, []);
 
-    const loadUsersFromExternalAPI = async() =>
-    {
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
-            .then(res => {
-                const persons = res.data;
-                setPersons(persons);
-            }).catch(err => {
-                toast.error("Failed to laod data");
-            });
-
-            UserService.getAll().then(res =>{
-                setPersons(res.data);
-            })
-    }
-
-    const loadUsers = async() =>{
+    const loadUsers = async () => {
         const persons = await UserService.getAll();
         setPersons(persons?.data);
     }
 
-    const selectClickHandler = (event, person) =>{
+    const selectClickHandler = (event, person) => {
         history.push({
             pathname: `/userForm`,
-            state: {user: person}
+            state: { user: person }
         });
     }
 
-    const addNewRecord = () => {
-        if(persons){
-            const newPerson = {
-                id: 11,
-                name: "test ",
-                username: "test",
-                email: "test@email.com",
-                phone: "123456789",
-                website: "test.com",
-                address: {
-                    street: "test street",
-                    city: "test city"
-                }
-            };
-            setPersons([...persons, newPerson]);
-        }
-    }
-
-    const deleteClickHandler = (event, id) =>{
-        UserService.remove(id).then(res=>{
-            //alert(res?.data.message);
-            toast.success(res?.data?.message, {
-                position: toast.POSITION.TOP_CENTER,
-                autoClose: 2000,
+    const handleConfirmDelete = () => {
+        UserService.remove(selectedPersonId)
+            .then((res) => {
+                toast.success(res?.data?.message, {
+                    position: toast.POSITION.TOP_CENTER,
+                    autoClose: 2000,
+                });
+                loadUsers();
+            })
+            .catch((error) => {
+                console.log(error);
+                toast.error("Failed to delete person"); // Displaying an error toast message using react-toastify
+            }).finally(() => {
+                setShowModal(false);
+                setSelectedPersonId(0);
             });
-            loadUsers();
-        })
     }
 
-    return(
-        <div className="container">
-            <h3>Persons page.</h3>
-            {/* <button className="btn btn-secondary btn-sm" style={{float: "left"}} onClick={addNewRecord}>Insert</button> */}
-            <Link to="/userForm" className="btn btn-primary btn" style={{float: "left"}}><AiOutlineUserAdd/></Link>
-            <table className="table w-100">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>FirstName</th>
-                        <th>LastName</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Select</th>
-                        <th>Del.</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                       persons.map((item, index) => {
-                        return(
-                            <tr key={index}>
-                                <td>{item.id}</td>
-                                <td>{item.firstName}</td>
-                                <td>{item.lastName}</td>
-                                <td>{item.email}</td>
-                                <td>{item.phone}</td>
-                                <td><button className="btn btn-success btn-sm" onClick={(event) => selectClickHandler(event, item)}><AiFillEdit /></button></td>
-                                <td><button className="btn btn-danger btn-sm" onClick={(event) => deleteClickHandler(event, item?.id)}><AiFillDelete /></button></td>
-                            </tr>
-                        );
-                       })
-                    }
-                </tbody>
-            </table>
-            <p>{person?.name}</p>
-        </div>
+    const deleteClickHandler = (event, id) => {
+        setSelectedPersonId(id);
+        setShowModal(true);
+    };
+
+    return (
+        <>
+            <div className="container">
+                <h3>Persons page.</h3>
+                {/* <button className="btn btn-secondary btn-sm" style={{float: "left"}} onClick={addNewRecord}>Insert</button> */}
+                <Link to="/userForm" className="btn btn-primary btn" style={{ float: "left" }}><AiOutlineUserAdd /></Link>
+                <table className="table w-100">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>FirstName</th>
+                            <th>LastName</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Select</th>
+                            <th>Del.</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            persons.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.id}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.email}</td>
+                                        <td>{item.phone}</td>
+                                        <td><button className="btn btn-success btn-sm" onClick={(event) => selectClickHandler(event, item)}><AiFillEdit /></button></td>
+                                        <td><button className="btn btn-danger btn-sm" onClick={(event) => deleteClickHandler(event, item?.id)}><AiFillDelete /></button></td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+                <p>{person?.name}</p>
+            </div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                {" "}
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to delete ?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                        No
+                    </Button>
+                    <Button variant="danger" onClick={handleConfirmDelete}>
+                        Yes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 
 };
