b77fe78c40b3ba594b3b8479d8e11b3d479343fe
charbel.f <charbel.f@i-stay.ltd>
2023-05-18 18:58:17 +0300

    fdf

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import UserPage from './components/user/UserPage';
 import Login from './components/Login';
 import TutorialList from './components/tutorials/TutorialList';
 import AddTutorial from './components/tutorials/AddTutrial';
-import { ToastContainer} from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import UserForm from './components/user/UserForm';
 import { useState } from 'react';
 
@@ -25,41 +25,53 @@ function App() {
   return (
     <div className="App">
       {isLoggedIn && (
-      <nav className='navbar navbar-expand' style={{backgroundColor: "#3498db"}}>
-        <a href="/" className="navbar-brand text-light">
-          CodePact <span>{'</>'}&#60;/&#62;</span>
-        </a>
-        <div className='navbar-nav mr-auto'>
-          <li className='nav-item'>
-            <Link to="/users" className="nav-link text-light">
-              Users
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link to="/addTutorial" className="nav-link text-light">
-              Add Tutorial
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link to="/tutorials" className="nav-link text-light">
-              Tutorials
-            </Link>
-          </li>
-        </div>
-      </nav>
+        <nav
+          className="navbar navbar-expand"
+          style={{ backgroundColor: "#3498db" }}
+        >
+          <a href="/" className="navbar-brand text-light">
+            CodePact <span>{"</>"}&#60;/&#62;</span>
+          </a>
+          <div className="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to="/users" className="nav-link text-light">
+                  Users
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/addTutorial" className="nav-link text-light">
+                  Add Tutorial
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/tutorials" className="nav-link text-light">
+                  Tutorials
+                </Link>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="btn btn-danger text-uppercase mx-auto smaller-logout-button"
+                  onClick={handleLogout}
+                >
+                  logout
+                </button>
+              </li>
+            </ul>
+          </div>
+        </nav>
       )}
       <div className='container mt-3'>
         <Switch>
           {isLoggedIn ? (
             <>
-            <Route exact path="/users" component={UserPage} />
-            <Route exact path="/tutorials" component={TutorialList} />
-            <Route exact path="/addTutorial" component={AddTutorial} />
-            <Route exact path="/userForm" component={UserForm} />
-            <button className='btn btn-danger text-uppercase'onClick={handleLogout}>logout</button>
+              <Route exact path="/users" component={UserPage} />
+              <Route exact path="/tutorials" component={TutorialList} />
+              <Route exact path="/addTutorial" component={AddTutorial} />
+              <Route exact path="/userForm" component={UserForm} />
             </>
-          ):(
-            <Route exact path="/" render={()=> <Login onLogin={handleLogin}/>}/>
+          ) : (
+            <Route exact path="/" render={() => <Login onLogin={handleLogin} />} />
           )}
         </Switch>
         <ToastContainer />
