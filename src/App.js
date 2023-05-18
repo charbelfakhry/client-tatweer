fc3a8301e61ef2b232237d64a46d402f96a42f7e
charbel.f <charbel.f@i-stay.ltd>
2023-05-18 18:38:03 +0300

    authenticate

--- src/App.js
+++ src/App.js
@@ -8,40 +8,59 @@ import AddTutorial from './components/tutorials/AddTutrial';
 import { ToastContainer} from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 import UserForm from './components/user/UserForm';
+import { useState } from 'react';
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  }
+
   return (
     <div className="App">
-      <nav className='navbar navbar-expand navbar-dark bg-dark'>
-        <a href="/" className="navbar-brand">
-          Tatweer
+      {isLoggedIn && (
+      <nav className='navbar navbar-expand' style={{backgroundColor: "#3498db"}}>
+        <a href="/" className="navbar-brand text-light">
+          CodePact <span>{'</>'}&#60;/&#62;</span>
         </a>
         <div className='navbar-nav mr-auto'>
           <li className='nav-item'>
-            <Link to="/users" className="nav-link">
+            <Link to="/users" className="nav-link text-light">
               Users
             </Link>
           </li>
           <li className='nav-item'>
-            <Link to="/addTutorial" className="nav-link">
+            <Link to="/addTutorial" className="nav-link text-light">
               Add Tutorial
             </Link>
           </li>
           <li className='nav-item'>
-            <Link to="/tutorials" className="nav-link">
+            <Link to="/tutorials" className="nav-link text-light">
               Tutorials
             </Link>
           </li>
         </div>
       </nav>
+      )}
       <div className='container mt-3'>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/users" component={UserPage} />
-          <Route exact path="/tutorials" component={TutorialList} />
-          <Route exact path="/addTutorial" component={AddTutorial} />
-          <Route exact path="/userForm" component={UserForm} />
+          {isLoggedIn ? (
+            <>
+            <Route exact path="/users" component={UserPage} />
+            <Route exact path="/tutorials" component={TutorialList} />
+            <Route exact path="/addTutorial" component={AddTutorial} />
+            <Route exact path="/userForm" component={UserForm} />
+            <button className='btn btn-danger text-uppercase'onClick={handleLogout}>logout</button>
+            </>
+          ):(
+            <Route exact path="/" render={()=> <Login onLogin={handleLogin}/>}/>
+          )}
         </Switch>
         <ToastContainer />
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,35 @@
 import React, {useState} from "react";
+import {RiUserFill, RiLockPasswordFill} from 'react-icons/ri';
+
+const Login = ({ onLogin }) => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
 
-const Login = props => {
+    const handleLogin = () =>{
+        if(username !== '' && password !== ''){
+            onLogin();
+        }
+    }
 
     return(
-        <div className="container">
-            <h1>This is the Login page</h1>
+        <div className="login-container">
+            <h2>Login</h2>
+            <hr style={{color: "#333", borderStyle: "dotted"}}/>
+            <div>
+                <RiUserFill className="input-icon" />
+                <input type="text" placeholder="Username" value={username} onChange={(e)=>setUsername(e.target.value)} />
+            </div>
+            <hr style={{color: "#333", borderStyle: "dotted"}}/>
+            <div>
+                <RiLockPasswordFill className="input-icon" />
+                <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+            </div>
+            <hr style={{color: "#333", borderStyle: "dotted"}}/>
+            <button className="btn btn-sm text-light fw-bold" style={{backgroundColor: "#3498db"}} onClick={handleLogin}>
+                Login
+            </button>
         </div>
     )
 }
