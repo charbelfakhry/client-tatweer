6d960c46481052653340ccab45406422e651b361
charbel.f <charbel.f@i-stay.ltd>
2023-05-27 11:25:26 +0300

    F

--- src/components/prodcut/ProductTable.js
+++ src/components/prodcut/ProductTable.js
@@ -3,86 +3,72 @@ import ProductService from "../../services/ProductService";
 import { useTable } from "react-table";
 
 const ProductTable = (props) => {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    useEffect(() => {
-        fetchProducts();
-    }, [])
-
-    const fetchProducts = () => {
-        ProductService.getProducts().then((response)=>{
-            console.log(response?.data);
-            //setProducts(response?.data === null ? [] : response?.data);
-        }).catch((e)=>{
-            console.log(e)
-        })
+  const fetchProducts = async () => {
+    try {
+      const response = await ProductService.getProducts();
+      console.log(response?.data);
+      setProducts(response?.data === null ? [] : response?.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const columns = useMemo(() => [
+  const columns = useMemo(
+    () => [
+      { Header: "Product Name", accessor: "product_name" },
+      { Header: "Des.", accessor: "product_desc" },
+      { Header: "Price", accessor: "product_price" },
+      { Header: "QTY", accessor: "product_qty" },
+      { Header: "Cat. Name", accessor: "category_name" },
+      { Header: "Supp. Name", accessor: "suplier_name" },
+    ],
+    []
+  );
 
-        {
-            Header: "Product Name",
-            accessor: "product_name"
-        },
-        {
-            Header: "Des.",
-            accessor: "product_desc"
-        },
-        {
-            Header: "Price",
-            accessor: "product_price"
-        },
-        {
-            Header: "QTY",
-            accessor: "product_qty"
-        },
-        {
-            Header: "Cat. Name",
-            accessor: "category_name"
-        },
-        {
-            Header: "Supp. Name",
-            accessor: "supplier_name"
-        },
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: products });
 
-    ])
-    const { headerGroups, rows, prepareRow, getTableBodyProps } = useTable({ columns, data: [] });
-
-    return (
-        <>
-            <h3>Products</h3>
-            <div className="container">
-                <table className="table table-striped table-bordered">
-                    <thead>
-                        {headerGroups.map((headerGroup) => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map((column) => (
-                                    <th {...column.getHeaderProps()}>
-                                        {column.render("Header")}
-                                    </th>
-                                ))}
-                            </tr>
-                        ))}
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {rows.map((row, i) => {
-                            prepareRow(row);
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map((cell) => {
-                                        return (
-                                            <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                                        );
-                                    })}
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h3>Products</h3>
+      <div className="container">
+        <table
+          className="table table-striped table-bordered"
+          {...getTableProps()}
+        >
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default ProductTable;
