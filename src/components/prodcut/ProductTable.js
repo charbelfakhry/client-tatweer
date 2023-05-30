59b7e22f54da66a53bbea7b54e5ae078a68fe445
charbel.f <charbel.f@i-stay.ltd>
2023-05-30 17:50:56 +0300

    materialUI table

--- src/components/prodcut/ProductTable.js
+++ src/components/prodcut/ProductTable.js
@@ -1,9 +1,21 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductService from "../../services/ProductService";
-import { useTable } from "react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  TableFooter,
+} from "@material-ui/core";
+
 
 const ProductTable = (props) => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   useEffect(() => {
     fetchProducts();
@@ -19,54 +31,72 @@ const ProductTable = (props) => {
     }
   };
 
-  const columns = useMemo(
-    () => [
-      { Header: "Product Name", accessor: "product_name" },
-      { Header: "Des.", accessor: "product_desc" },
-      { Header: "Price", accessor: "product_price" },
-      { Header: "QTY", accessor: "product_qty" },
-      { Header: "Cat. Name", accessor: "category_name" },
-      { Header: "Supp. Name", accessor: "suplier_name" },
-    ],
-    []
-  );
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data: products });
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, products.length - page * rowsPerPage);
 
   return (
     <>
       <h3>Products</h3>
-      <div className="container">
-        <table
-          className="table table-striped table-bordered"
-          {...getTableProps()}
-        >
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <TableContainer>
+        <Table aria-label="product table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Product Name</TableCell>
+              <TableCell>Des.</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>QTY</TableCell>
+              <TableCell>Cat. Name</TableCell>
+              <TableCell>Supp. Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell>{product.product_name}</TableCell>
+                  <TableCell>{product.product_desc}</TableCell>
+                  <TableCell>{product.product_price}</TableCell>
+                  <TableCell>{product.product_qty}</TableCell>
+                  <TableCell>{product.category_name}</TableCell>
+                  <TableCell>{product.supplier_name}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableContainer className="w-100">
+        <TableFooter className="footer text-center w-100">
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 50]}
+              colSpan={6}
+              count={products.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { "aria-label": "rows per page" },
+                native: true,
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              labelRowsPerPage="Rows per page:"
+              labelDisplayedRows={({ from, to, count }) =>
+                `${from}-${to} of ${count}`
+              }
+            />
+          </TableRow>
+        </TableFooter>
+      </TableContainer>
     </>
   );
 };
