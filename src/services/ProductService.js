a77b6ff0aecee49841217f5b91dbdb8d09f572fc
charbel.f <charbel.f@i-stay.ltd>
2023-05-30 18:17:18 +0300

    f

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -5,8 +5,24 @@ const getProducts = () =>
     return http.get(`/products/getProducts`);
 }
 
+const create = (data) =>{
+    return http.post(`/products/insertProduct`, data);
+}
+
+const update = (data) => {
+    return http.post(`/products/updateProduct`, data);
+}
+
+const remove = (id) =>{
+    return http.post(`/products/deleteProduct`, {id});
+    //return http.delete(`/users/deleteUser/${id}`);
+}
+
 const ProductService = {
     getProducts,
+    create,
+    update,
+    remove,
 }
 
 export default ProductService;
